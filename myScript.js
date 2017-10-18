5dc583e506a0f5c0f844b323021e9c6654510737
Meghan King <mfking@uvm.edu>
2017-10-18 12:31:39 -0400

    Basic Setup files

---
+++ myScript.js
@@ -0,0 +1,79 @@
+
+/*var cols = ["col1", "col2", "col3", "col4", "col5", "col6"];
+var clicked = false;
+var theParent = document.querySelector("#row");
+document.getElementById("col1").addEventListener("click", doSomething);
+
+function doSomething() {
+	var grid = document.getElementById("col1");
+		if(clicked == false){
+			grid.style.width = '66.66%';
+			grid.style.height = '150%';
+			clicked = true;
+		}
+		else{
+
+			grid.style.width = '33.33%';
+			grid.style.height = '100%';
+			clicked = false;
+		}
+
+} */
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+var add = document.getElementsByClassName("addBtn");
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
