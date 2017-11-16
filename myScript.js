25d4e27f7345e95d2aa343d1c753715d5c8c967e
Meghan King <mfking@uvm.edu>
2017-11-15 19:36:32 -0500

    To Do list
    To do list is almost done, it has the general idea and local storage
    (meaning if you leave the page and come back your to-do list will
    remain the same). starting on the idea of widgets, will continue work

--- myScript.js
+++ myScript.js
@@ -1,10 +1,44 @@
 
-
+/******************************* To Do List Script *******************************/
 var element = document.querySelector('.addBtn');
-                                      
-element.addEventListener("click", function(e) {
-    newElement();
-}, false);
+if(element){
+  console.log("ELEMENT1");
+    element.addEventListener("click", function(e) {
+      newElement();
+      }, false);
+} 
+
+var element = document.querySelector('.toDo');
+if(element){
+  console.log("ELEMENT2");
+    element.addEventListener("click", function(e) {
+      showToDoList();
+      }, false);
+}   
+                               
+
+var items = [];
+var checks = [];
+
+
+//add items from local storage
+items = JSON.parse(localStorage.getItem("items"));
+checks = JSON.parse(localStorage.getItem("checks"));
+var i;
+for(i = 0; i < items.length; i++){
+  var li = document.createElement("li");
+  var itemValue = items[i];
+  var t = document.createTextNode(itemValue);
+  li.appendChild(t);
+  var ul = document.getElementById("myUL");
+  if(ul){
+    ul.appendChild(li);
+  }
+
+  if(checks.indexOf(itemValue) > -1){
+      li.classList.toggle('checked');
+  }
+}
 
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
@@ -20,20 +54,44 @@ for (i = 0; i < myNodelist.length; i++) {
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
+console.log("Close length: " + close.length);
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
+
+    //remove the element from the display
     var div = this.parentElement;
     div.style.display = "none";
+  
+    //remove the element from the items list (storage)
+    var string = this.parentElement.innerText;
+    var index = items.indexOf(string.substring(0, string.length-1));
+    if(index > -1){
+      items.splice(index, 1);
+    }
+    localStorage.setItem("items", JSON.stringify(items));
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+if(list){
+  list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+
+      var str = ev.target.innerText;
+      if(ev.target.className == 'checked'){
+        checks.push(str.substring(0, str.length - 1));
+      } else {
+        var index = checks.indexOf(str.substring(0, str.length-1));
+        if(index > -1){
+          checks.splice(index, 1);
+        }
+      }
+      localStorage.setItem("checks", JSON.stringify(checks));
+    }
+  }, false);
+}
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
@@ -46,6 +104,10 @@ function newElement() {
   } else {
     document.getElementById("myUL").appendChild(li);
   }
+   
+  items.push(inputValue);
+  localStorage.setItem("items", JSON.stringify(items));
+
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
@@ -60,4 +122,13 @@ function newElement() {
       div.style.display = "none";
     }
   }
+}
+
+function showToDoList() {
+    var list = document.getElementById("toDoList");
+    if(list.style.display == "none"){
+      list.style.display = "block";
+    } else {
+      list.style.display = "none";
+    }
 }
