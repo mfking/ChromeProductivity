54e5f64094ec293b17c78f092624782a95b4df54
lstuntz <lstuntz@uvm.edu>
2017-12-08 03:44:53 -0500

    productivity functionality

--- js/myScript.js
+++ js/myScript.js
@@ -1751,7 +1751,66 @@ $('#endTimeEventMin').focusout(function() {
 
 
 /******************************* Productivity Script *******************************/
+function findMaxTime(times){
+  if($.isEmptyObject(times)){
+    return -1;
+  }
+  var maxIndex = 0;
+  $.each(times.timing, function(i, v){
+    if(v.time > times.timing[maxIndex].time){
+      maxIndex = i;
+    }
+  });
+  return maxIndex;
+}
 
+//add items from local storage
+var timings = JSON.parse(localStorage.getItem("timing"));
+var link_by_time;
+if(!timings){
+  timings = [];
+}
+else{
+
+  //sort the timings
+  var timingCopy = {'timing': []};
+  $.each(timings.timing, function(i, v){
+    timingCopy.timing.push({
+      'title': v.title,
+      'time': v.time
+    });
+  });
+
+  var sortedTimes = {'timing': []};
+  var i;
+  for(i = 0; i < timings.timing.length; ++i){
+    var x = findMaxTime(timingCopy);
+    sortedTimes.timing.push({
+      'title': timingCopy.timing[x].title,
+      'time': timingCopy.timing[x].time
+    });
+    timingCopy.timing.splice(x, 1);
+  }
+
+
+  $.each(sortedTimes.timing, function(i, v) {
+    var li = document.createElement("li");
+    var mins = v.time / 60;
+    var pr = v.title + " - " + parseFloat(mins).toFixed(2) + " minutes";
+    var t = document.createTextNode(pr);
+    li.appendChild(t);
+    li.className = "toDoItem";
+    var ul = document.getElementById("prod");
+    if(ul){
+      ul.appendChild(li);
+    }
+
+    console.log("m" + mins);
+
+
+ 
+    });
+}
 
 // click event
 var element = document.querySelector('.prodBtn');
